e9a1afc80be995d8dc8e9f988f3a3232b077b902
Jamie Talbot <code@jamietalbot.com>
2011-01-16 09:24:39 +1000

    * Add specifications to matrix module. * Add row/2, to return a single row from a matrix. * Add column/2, to return a single column from a matrix.

--- src/matrix.erl
+++ src/matrix.erl
@@ -15,46 +15,63 @@
     multiply/2,
     sum/1,
     dimensions/1,
-    element_set/4
+    element_set/4,
+    column/2,
+    row/2
 ]).
 
+
+%% File: "matrix.erl"
+%% ------------------
+-type matrix() :: [[any(), ...]].
+-type int_matrix() :: [[integer(), ...]].
+-type num_matrix() :: [[number(), ...]].
+
 % Convenience function to create a square matrix.
+-spec new(integer()) -> [[any()]].
 new(Size) ->
     new(Size, Size).
 
 % Creates a square matrix with all elements initialised to 0, or the value returned by the specified generator function.
+-spec new(pos_integer(), fun((pos_integer(), pos_integer(), pos_integer(), pos_integer()) -> any()) | pos_integer()) -> matrix().
 new(Size, ContentGenerator) when is_function(ContentGenerator, 4) ->
     new(Size, Size, ContentGenerator);
-
-% Creates a rectangular matrix initialised to 0.
 new(Columns, Rows) ->
+% Creates a rectangular matrix initialised to 0.
     [[0 || _ <- lists:seq(1, Columns)] || _ <- lists:seq(1, Rows)].
 
 % Creates a rectangular matrix using the specified function to generate elements.
+-spec new(pos_integer(), pos_integer(), fun(pos_integer(), pos_integer(), pos_integer(), pos_integer() -> any())) -> matrix().
 new(Columns, Rows, ContentGenerator) ->
     [[ContentGenerator(Column, Row, Columns, Rows) || Column <- lists:seq(1, Columns)] || Row <- lists:seq(1, Rows)].
 
 % Convenience function to create a random matrix of specified dimensions, with an upper bound of MaxValue for elements.
+-spec random(pos_integer(), pos_integer(), pos_integer()) -> int_matrix().
 random(Columns, Rows, MaxValue) ->
     random:seed(erlang:now()),
     new(Columns, Rows, fun(_, _, _, _) -> random:uniform(MaxValue) end).
 
 % Convenience function to create a sequential matrix where element values ascend.
+-spec sequential(pos_integer(), pos_integer()) -> int_matrix().
 sequential(Columns, Rows) ->
     new(Columns, Rows, fun(Column, Row, CS, _) -> CS * (Row - 1) + Column end).
 
 % Convenience function creates an identity matrix of the supplied size.
+-spec identity(pos_integer()) -> int_matrix().
 identity(Size) ->
     new(Size, Size, fun(Column, Row, _, _) -> case Column of Row -> 1; _ -> 0 end end).
 
 % Counts the elements in a matrix.
+-spec element_count(matrix()) -> pos_integer().
 element_count(Matrix) ->
     length(Matrix) * length(lists:nth(1, Matrix)).
 
 % Returns the element of a matrix in the specified position.
+-spec element_at(pos_integer(), pos_integer(), matrix()) -> any().
 element_at(Column, Row, Matrix) ->
     lists:nth(Column, lists:nth(Row, Matrix)).
 
+-spec element_set(pos_integer(), pos_integer(), any(), matrix()) -> matrix().
 element_set(ElementColumn, ElementRow, Value, Matrix) ->
     {Width, Height} = matrix:dimensions(Matrix),
     matrix:new(Width, Height, fun(Column, Row, _, _) ->
@@ -66,10 +83,20 @@ element_set(ElementColumn, ElementRow, Value, Matrix) ->
             end
         end).
 
+-spec row(pos_integer(), matrix()) -> [any(), ...]
+row(Row, Matrix) ->
+    lists:nth(Row, Matrix).
+
+-spec column(pos_integer(), matrix()) -> [any(), ...]
+column(Column, Matrix) ->
+    [lists:nth(Column, Row) || Row <- Matrix].
+
+-spec dimensions(matrix()) -> {pos_integer(), pos_integer()}.
 dimensions(Matrix) ->
     {length(lists:nth(1, Matrix)), length(Matrix)}.
 
 % Returns the product of two matrices.
+-spec mutiply(num_matrix(), num_matrix()) -> num_matrix().
 multiply(A, B) ->
     Count = length(lists:nth(1, A)),
     matrix:new(Count, length(A), fun(Column, Row, _, _) ->
@@ -78,9 +105,11 @@ multiply(A, B) ->
     ).
 
 % Auxiliary function to calculate the matrix product for a single element.
+-spec sum_product(pos_integer(), pos_integer(), pos_integer(), num_matrix(), num_matrix()) -> number().
 sum_product(Count, Column, Row, A, B) ->
     sum_product(Count, Column, Row, A, B, 0).
 
+-spec sum_product(non_neg_integer(), pos_integer(), pos_integer(), num_matrix(), num_matrix(), number()) -> number().
 sum_product(0, _, _, _, _, Total) ->
     Total;
 sum_product(Count, Column, Row, A, B, Total) ->
@@ -89,17 +118,20 @@ sum_product(Count, Column, Row, A, B, Total) ->
 
 % Returns the matrix raised to the specified power.  Naive implementation that just
 % recursively multiplies.
+-spec power(num_matrix(), pos_integer()) -> num_matrix().
 power(Matrix, 1) ->
     Matrix;
 power(Matrix, N) ->
     power(Matrix, N - 1, multiply(Matrix, Matrix)).
 
+-spec power(num_matrix(), pos_integer(), num_matrix()) -> num_matrix().
 power(_, 1, Result) ->
     Result;
 power(Matrix, N, Partial) ->
     power(Matrix, N - 1, multiply(Matrix, Partial)).
 
 % Returns the given matrix with each element multiplied by the supplied integer.
+-spec scalar_multiply(num_matrix(), integer()) -> num_matrix().
 scalar_multiply(Matrix, 1) ->
     Matrix;
 scalar_multiply(Matrix, Scalar) ->
@@ -109,6 +141,7 @@ scalar_multiply(Matrix, Scalar) ->
     ).
 
 % Adds two matrices together.
+-spec add(num_matrix(), num_matrix()) -> num_matrix().
 add(A, B) ->
     matrix:new(length(lists:nth(1, A)), length(A), fun(Column, Row, _, _) ->
             element_at(Column, Row, A) + element_at(Column, Row, B)
@@ -116,5 +149,6 @@ add(A, B) ->
     ).
 
 % Returns the sum of all the elements of a matrix.
+-spec sum(num_matrix()) -> number().
 sum(Matrix) ->
     lists:sum(lists:flatten(Matrix)).
